a39998459ad32e4446b43b85e7e9de1ff62510c5
Jonathan Wright <j.wright72127@gmail.com>
2021-08-19 15:08:16 -0600

    Added Bottom Navbar and Renamed Top Navbar.

--- foodiechews.client/src/App.vue
+++ foodiechews.client/src/App.vue
@@ -1,22 +1,12 @@
 <template>
   <header>
-    <Navbar />
+    <NavbarMobileTop />
   </header>
   <main>
     <router-view />
   </main>
   <footer>
-    <div class="d-flex flex-row bg-primary text-light text-center p-4">
-      <div class="col-3">
-        🔍
-      </div>
-      <div class="col-6">
-        Random
-      </div>
-      <div class="col-3">
-        🏠
-      </div>
-    </div>
+    <NavbarMobileBottom />
   </footer>
 </template>
 
---
+++ foodiechews.client/src/components/NavbarMobileBottom.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="d-flex flex-row bg-primary text-light text-center p-4">
+    <div class="col-3">
+      🔍
+    </div>
+    <div class="col-6">
+      Random
+    </div>
+    <div class="col-3">
+      🏠
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
