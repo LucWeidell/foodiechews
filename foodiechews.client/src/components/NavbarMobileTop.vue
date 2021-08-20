494dd21cc20543cc0f5b7d757023982857a35838
SamanthaBullington <Samantha.J.Bullington@gmail.com>
2021-08-20 16:05:56 -0600

    added navbar about

--- foodiechews.client/src/components/NavbarMobileTop.vue
+++ foodiechews.client/src/components/NavbarMobileTop.vue
@@ -24,6 +24,11 @@
             About
           </router-link>
         </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Profile' }" class="nav-link">
+            My Profile
+          </router-link>
+        </li>
       </ul>
       <span class="navbar-text">
         <button
