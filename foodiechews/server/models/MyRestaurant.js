eed94c2706d3b3d2fbbfcb7be56a8c0325e3f6a0
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-20 16:21:20 -0600

    restraunts notes

--- foodiechews/server/models/MyRestaurant.js
+++ foodiechews/server/models/MyRestaurant.js
@@ -4,9 +4,13 @@ const ObjectId = Schema.Types.ObjectId
 
 const MyRestaurant = new Schema(
   {
-    yelp: { type: String, required: true },
+    /** This is the yelpId associated with this restaurant */
+    yelpID: { type: String, required: true },
+    /** This is the Array holding all Visit Models for a Restaurant */
     visits: { type: Array, required: true },
+    /** This is the String name for a Restaurant */
     name: { type: String, required: true },
+    /** This is the String city for a Restaurant */
     restCity: { type: String, required: true },
     tag: { type: String, enum: ['favorite', 'none'], default: 'none' },
     ProfileId: { type: ObjectId, ref: 'Profile', required: true }
