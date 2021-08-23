e262ae6566b3d5e30815b31bf3ac90c76ba740f7
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-23 05:50:48 -0600

    + edit, srch acc by id: accnt holds everythng now

--- foodiechews/server/controllers/AccountController.js
+++ foodiechews/server/controllers/AccountController.js
@@ -1,6 +1,7 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
-import { accountService } from '../services/AccountService'
+// import { accountsDbService } from '../services/AccountDbService'
 import BaseController from '../utils/BaseController'
+import { accountService } from '../services/AccountService'
 
 export class AccountController extends BaseController {
   constructor() {
@@ -8,6 +9,9 @@ export class AccountController extends BaseController {
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
+      .get('/:id', this.getById)
+      .put('/:id', this.edit)
+      // .post('', this.create)
   }
 
   async getUserAccount(req, res, next) {
@@ -18,4 +22,70 @@ export class AccountController extends BaseController {
       next(error)
     }
   }
+
+  async edit(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      const profile = await accountService.edit(req.userInfo, req.body)
+      res.send(profile)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Sends all profile with Id to a client by request
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+       * @param {import('express').NextFunction} next
+   */
+  async getById(req, res, next) {
+    try {
+      const profile = await accountService.getById(req.params.id)
+      res.send(profile)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  // /**
+  //      * Adds a data to a client by request
+  //      * @param {import('express').Request} req
+  //      * @param {import('express').Response} res
+  //      * @param {import('express').NextFunction} next
+  //      */
+  // async create(req, res, next) {
+  //   try {
+  //     const profile = await accountsDbService.create(req.body)
+  //     res.send(profile)
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+
+  // async edit(req, res, next) {
+  //   try {
+  //     req.body.authorizeId = req.userInfo.id
+  //     req.body.id = req.params.id
+  //     const profile = await accountsDbService.edit(req.body)
+  //     res.send(profile)
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+
+  // /**
+  //  * Sends all profile with Id to a client by request
+  //  * @param {import('express').Request} req
+  //  * @param {import('express').Response} res
+  //      * @param {import('express').NextFunction} next
+  //  */
+  // async getById(req, res, next) {
+  //   try {
+  //     const profile = await accountsDbService.getById(req.params.id)
+  //     res.send(profile)
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
 }
--- foodiechews/server/models/Account.js
+++ foodiechews/server/models/Account.js
@@ -6,7 +6,14 @@ export const AccountSchema = new Schema(
     subs: [{ type: String, unique: true }],
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
-    picture: { type: String }
+    picture: { type: String },
+    // Location is an array of strings
+    location: { type: Array, default: [] },
+    // myEats is an array of MyRestaurants objects (should be empty when profile is created)
+    myEats: { type: Array, default: [] },
+    pendingRestraunt: { type: Object, default: {} },
+    activeLocation: { type: String, default: '' },
+    noDupes: { type: Boolean, default: true }
     // NOTE If you wish to add additional public properties for Accounts do so here
   },
   { timestamps: true, toJSON: { virtuals: true } }
