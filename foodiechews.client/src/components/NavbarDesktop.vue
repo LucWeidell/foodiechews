c343921d1b00b4748062d3145fa693733be600f7
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-23 05:03:39 -0600

    Just added stup for navbar desktop

---
+++ foodiechews.client/src/components/NavbarDesktop.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="NavbarDesktop">
+    <!-- FIXME need the navbar for desktop here: mobile versio has account name go offscreen -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarDesktop',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
