a483b3295874c39f74c6f9c1e84da26d9bdcaa82
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-23 06:36:23 -0600

    visit getAll works: need to test (should be done)

--- foodiechews/server/controllers/VisitsController.js
+++ foodiechews/server/controllers/VisitsController.js
@@ -7,7 +7,18 @@ export class VisitsController extends BaseController {
     super('api/visits')
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
+      .get('', this.getAll)
       .post('', this.create)
+      .put('/:id', this.edit)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const visits = await visitsService.getAll(req.query)
+      res.send(visits)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async create(req, res, next) {
@@ -15,6 +26,16 @@ export class VisitsController extends BaseController {
       req.body.creatorId = req.userInfo.id
       // NOTE: MyRestaurant id needs to be passed via client(?)
       const visit = await visitsService.create(req.body)
+      res.send(visit)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      const visit = await visitsService.edit(req.user, req.body)
       res.send(visit)
     } catch (error) {
       next(error)
--- foodiechews/server/services/VisitsService.js
+++ foodiechews/server/services/VisitsService.js
@@ -1,4 +1,5 @@
 import { dbContext } from '../db/DbContext'
+import { BadRequest, Forbidden } from '../utils/Errors'
 class VisitsService {
   async getAll(query = {}) {
     const visits = await dbContext.Visits.find(query)
@@ -9,6 +10,18 @@ class VisitsService {
     const visit = await dbContext.Visits.create(body)
     return await dbContext.Visits.findById(visit._id)
       .populate('restaurant')
+  }
+
+  async edit(user, body) {
+    if (!user.id === body.accountId) {
+      throw new Forbidden('Not your visit:')
+    }
+    const visit = await dbContext.Visits.findOneAndUpdate(
+      { _id: body.id },
+      body,
+      { runValidators: true, setDefaultsOnInsert: true, new: true }
+    )
+    return visit
   }
 }
 
