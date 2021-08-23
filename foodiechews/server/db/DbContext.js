23842916b7b196c06b7c742daebb1ddbd95f132f
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-23 05:53:04 -0600

    Profiles model and db context removed

--- foodiechews/server/db/DbContext.js
+++ foodiechews/server/db/DbContext.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
-import ProfileSchema from '../models/Profile'
+// import ProfileSchema from '../models/Profile'
 import MyRestaurantSchema from '../models/MyRestaurant'
 import TotalCityRestSchema from '../models/TotalCityRest'
 import YelpRestaurantSchema from '../models/YelpRestaurant'
@@ -10,7 +10,6 @@ import VisitSchema from '../models/Visit'
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
-  Profiles = mongoose.model('Profile', ProfileSchema)
   MyRestaurants = mongoose.model('MyRestaurant', MyRestaurantSchema)
   TotalCityRests = mongoose.model('TotalCityRest', TotalCityRestSchema)
   YelpRestaurants = mongoose.model('YelpRestaurant', YelpRestaurantSchema)
