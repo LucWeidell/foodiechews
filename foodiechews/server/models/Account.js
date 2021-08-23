0d9376bb50afbc9bae15e0b473305093b3aa6bdc
CyberTomB <afntwrk.guyrpg@gmail.com>
2021-08-23 15:50:48 -0600

    added notes from sprint plan meeting

--- foodiechews/server/models/Account.js
+++ foodiechews/server/models/Account.js
@@ -1,16 +1,24 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
+// NOTE: This is how subdocuments work! Reference this!
+const locationSchema = new Schema({
+  city: { type: String, required: true },
+  state: { type: String, required: true }
+},
+{ timestamps: true, toJSON: { virtuals: true } })
+
 export const AccountSchema = new Schema(
   {
     subs: [{ type: String, unique: true }],
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
     picture: { type: String },
-    // Location is an array of strings
-    location: { type: Array, default: [] },
+    location: [locationSchema],
+    // TODO: Use subdocument instead of array
     // myEats is an array of MyRestaurants objects (should be empty when profile is created)
     myEats: { type: Array, default: [] },
+    // TODO: Also use a subdoc to define the object props
     pendingRestraunt: { type: Object, default: {} },
     activeLocation: { type: String, default: '' },
     noDupes: { type: Boolean, default: true }
