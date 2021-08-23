354cc4334edd10946d8a35548109f7001f8560d9
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-23 11:37:34 -0600

    TotalCity to city and state

--- foodiechews/server/models/TotalCityRest.js
+++ foodiechews/server/models/TotalCityRest.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const TotalCityRest = new Schema(
   {
     city: { type: String, unique: true, lowercase: true, required: true },
+    state: { type: String, uppercase: true, required: true },
     totalNum: { type: Number, required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
