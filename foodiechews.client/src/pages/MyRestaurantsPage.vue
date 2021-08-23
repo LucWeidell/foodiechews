dc7d68b3969dedf6c4e7bbcb4a1f04fa9f7d44ee
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-23 05:08:23 -0600

    change bg-img on myrests for non-mobile

--- foodiechews.client/src/pages/MyRestaurantsPage.vue
+++ foodiechews.client/src/pages/MyRestaurantsPage.vue
@@ -17,4 +17,11 @@ export default {
   background-position: center;
   background-size: cover;
 }
+@media only screen and (min-width: 770px) {
+  #bg-img {
+    background-image: url('https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1867&q=80');
+    background-size: cover;
+    background-position: center;
+  }
+}
 </style>
