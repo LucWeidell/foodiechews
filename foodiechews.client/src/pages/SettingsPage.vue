9c193c84294058382b069840ae0c5fb8a503d8cb
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-24 16:26:05 -0600

    settings work

--- foodiechews.client/src/pages/SettingsPage.vue
+++ foodiechews.client/src/pages/SettingsPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid">
     <div class="row justify-content-around py-3">
-      <div class="col-6">
+      <div class="col-6 card bg-bootSec text-dark">
         <form @submit.prevent="editAccount(account)">
           <div class="form-group form-check">
             <input type="checkbox" name="nodupes" id="nodupes" class="form-check-input" v-model="account.noDupes">
@@ -12,18 +12,31 @@
             <label for="showRestaurants" class="form-check-label">Show My Restaurant History</label>
             <small id="showRestaurantsHelp" class="form-text text-muted"><em>Only show cities on my profile page.</em></small>
           </div>
-          <div class="form-group">
-            <label for="activeLocationCity">City:</label>
-            <input class="form-control" id="activeLocation" type="text" placeholder="New York City" v-model="account.activeLocation.city">
-            <select name="activeLocationState" id="activeLocationState" v-model="account.activeLocation.state">
-              <option :value="state" v-for="(state, index) in states" :key="index">
-                {{ state }}
-              </option>
-            </select>
+          <div class="row">
+            <div class="col-sm-12 col-md-6">
+              <div class="form-group">
+                <label for="activeLocationCity">City:</label>
+                <input class="form-control"
+                       id="activeLocationCity"
+                       type="text"
+                       placeholder="New York City"
+                       v-model="account.activeLocation.city"
+                >
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-2 pb-1">
+              <select name="activeLocationState" id="activeLocationState" v-model="account.activeLocation.state">
+                <option :value="state" v-for="(state, index) in states" :key="index">
+                  {{ state }}
+                </option>
+              </select>
+            </div>
+            <div class="col-sm-12 col-md-2 mb-1">
+              <button type="submit" class="btn btn-secondary btn-sm">
+                + City
+              </button>
+            </div>
           </div>
-          <button type="submit">
-            SAVE
-          </button>
         </form>
       </div>
     </div>
@@ -65,8 +78,13 @@ export default {
           Pop.toast(error, 'error')
         }
       }
-
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .bg-bootSec {
+    color: rgb(204, 204, 204)
+  }
+</style>
