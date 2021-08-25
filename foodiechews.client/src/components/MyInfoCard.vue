4ad7bdfb3b6d6bcad5e6084bc4370efbea561eac
CyberTomB <afntwrk.guyrpg@gmail.com>
2021-08-25 17:05:42 -0600

    myinfocard populates data

--- foodiechews.client/src/components/MyInfoCard.vue
+++ foodiechews.client/src/components/MyInfoCard.vue
@@ -1,3 +1,24 @@
 <template>
-  This is my info card
+  <p>{{ visits[0].review }}</p>
 </template>
+
+<script>
+import { computed, onMounted, watchEffect } from '@vue/runtime-core'
+import { visitsService } from '../services/VisitsService'
+import { useRoute } from 'vue-router'
+import { AppState } from '../AppState'
+export default {
+  setup() {
+    const route = useRoute()
+    watchEffect(async() => {
+      if (AppState.myRestaurants.length !== 0) {
+        const myRestaurant = AppState.myRestaurants.find(r => r.yelpId === route.params.yelpId)
+        await visitsService.getVisitsByMyRestaurantId(myRestaurant.id)
+      }
+    })
+    return {
+      visits: computed(() => AppState.visits)
+    }
+  }
+}
+</script>
