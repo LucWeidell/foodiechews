cd618bcda653069e13f9d410a096d317d2da57f1
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-25 11:32:38 -0600

    Serach Page Routing: Other function to router-link

--- foodiechews.client/src/components/NavbarMobileBottom.vue
+++ foodiechews.client/src/components/NavbarMobileBottom.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="d-flex flex-row bg-primary text-light text-center align-items-center m-0">
     <div class="col-4 py-1">
-      <img src="../assets/img/search.png"
-           class="p-0 action"
-           alt="Random Button"
-           width="50"
-      >
+      <router-link :to="{name: 'SearchPage', params: {id: state.account.id}}">
+        <img src="../assets/img/search.png"
+             class="p-0 action"
+             alt="Search Button"
+             width="50"
+        >
+      </router-link>
     </div>
     <div class="col-4 py-1 border-right border-left border-dark">
       <img src="../assets/img/random.png"
