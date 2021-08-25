0a5bd14f1b49f864cbfce3ff4dcd90274a9ef6f7
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-25 16:57:00 -0600

    Search page loads new card w/filter

--- foodiechews.client/src/services/YelpRestaurantsService.js
+++ foodiechews.client/src/services/YelpRestaurantsService.js
@@ -27,10 +27,16 @@ class YelpRestaurantsService {
           break
         case 'search':
         // console.log(id, query)
-          res = await api.get(`/api/yelpRestaurants/random?location=${locCity + locState}&open_now=${AppState.account.showOnlyOpen}`)
+          if (query.price && query.categories) {
+            res = await api.get(`/api/yelpRestaurants/search?categories=${query.categories}&price=${query.price}&location=${locCity + locState}&open_now=${AppState.account.showOnlyOpen}`)
+          } else if (query.categories) {
+            res = await api.get(`/api/yelpRestaurants/search?categories=${query.categories}&location=${locCity + locState}&open_now=${AppState.account.showOnlyOpen}`)
+          } else if (query.price) {
+            res = await api.get(`/api/yelpRestaurants/search?price=${query.price}&location=${locCity + locState}&open_now=${AppState.account.showOnlyOpen}`)
+          } else {
+            res = await api.get(`/api/yelpRestaurants/search?location=${locCity + locState}&open_now=${AppState.account.showOnlyOpen}`)
           // console.log('the res:', res.data)
-          AppState.activeRestaurant = res.data
-          AppState.loading = false
+          }
           break
         // NOTE had default as false before but we can identify /:id as an id for a string compare
         // REVIEW could check the length of the string: but switches may not support
