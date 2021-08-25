16ba2b38c931768655ae6de6ab9095cfac9dbe85
SamanthaBullington <Samantha.J.Bullington@gmail.com>
2021-08-25 14:48:56 -0600

    visit modal update

--- foodiechews.client/src/components/VisitModal.vue
+++ foodiechews.client/src/components/VisitModal.vue
@@ -8,27 +8,66 @@
        aria-hidden="true"
   >
     <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="staticBackdropLabel">
-            Modal title
-          </h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+      <form @submit.prevent="submitVisit">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">
+              Modal title
+            </h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="review">Review</label>
+              <input type="text"
+                     class="form-control"
+                     id="review"
+                     aria-describedby="review"
+                     placeholder="Tell us about your visit..."
+                     v-model="state.newVisit.review"
+              >
+              <div class="input-group-checkbox">
+                Favorite:
+                <input type="checkbox"
+                       aria-label="Favorite"
+                       v-model="state.newVisit.favorite"
+                >
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">
+              Close
+            </button>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </div>
         </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">
-            Close
-          </button>
-          <button type="button" class="btn btn-primary">
-            Understood
-          </button>
-        </div>
-      </div>
+      </form>
     </div>
   </div>
 </template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+export default {
+  setup() {
+    const state = reactive({
+      newVisit: {
+        review: '',
+        favorite: false
+      }
+    })
+
+    return {
+      state,
+      submitVisit() {
+        console.log('modal logs', state.newVisit)
+      }
+    }
+  }
+}
+</script>
