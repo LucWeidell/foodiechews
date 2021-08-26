2d3eeab95ddeaecddf7316774584b4b8aac69cc6
CyberTomB <afntwrk.guyrpg@gmail.com>
2021-08-26 11:48:14 -0600

    myinfocard functionality

--- foodiechews.client/src/components/MyInfoCard.vue
+++ foodiechews.client/src/components/MyInfoCard.vue
@@ -1,22 +1,36 @@
 <template>
-  <p>{{ visits[0].review }}</p>
+  <h1>{{ state.myRestaurant.tag === 'none' ? '' : '*' }}</h1>
+  <div class="div" v-if="visits.length === 0">
+    <em>No Review Yet</em>
+  </div>
+  <div v-else>
+    <p v-for="v in visits" :key="v.id">
+      {{ v.review }}
+    </p>
+  </div>
 </template>
 
 <script>
-import { computed, onMounted, watchEffect } from '@vue/runtime-core'
+import { computed, onMounted, reactive, watchEffect } from '@vue/runtime-core'
 import { visitsService } from '../services/VisitsService'
 import { useRoute } from 'vue-router'
 import { AppState } from '../AppState'
 export default {
   setup() {
     const route = useRoute()
+    const state = reactive({
+      myRestaurant: {}
+    })
+    // TODO: When moving from a yelpID details page back to "random," the "myRestaurants" triggers an undefined error but doesn't seem to block functionality.
     watchEffect(async() => {
       if (AppState.myRestaurants.length !== 0) {
         const myRestaurant = AppState.myRestaurants.find(r => r.yelpId === route.params.yelpId)
+        state.myRestaurant = myRestaurant
         await visitsService.getVisitsByMyRestaurantId(myRestaurant.id)
       }
     })
     return {
+      state,
       visits: computed(() => AppState.visits)
     }
   }
