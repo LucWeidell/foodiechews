f81d720d4f641e74b3b613215d9f0f20725f0912
Luc Weidell <>
2021-09-23 11:43:33 -0600

    fix env

--- foodiechews.client/src/env.js
+++ foodiechews.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'cyberdack.us.auth0.com'
-export const audience = 'https://foodiechews.com'
-export const clientId = 'r4azoIcLXifaYprZer1F2kumlLifPwla'
+export const domain = 'dev-4ed7g57f.us.auth0.com'
+export const audience = 'https://foodie-chews.com'
+export const clientId = 'a3SsgvPnbZmiSPlywfZpTJTnoB27EWuv'
