2418f6466e27ce5b75d369d85c5360d27f7d7850
Luc Weidell <>
2021-09-23 12:42:57 -0600

    rating works

--- foodiechews.client/src/utils/RatingsUtil.js
+++ foodiechews.client/src/utils/RatingsUtil.js
@@ -5,37 +5,38 @@ class RatingsUtil {
    * @returns {String} (image URL for Yelp Stars)
    */
   getStars(rating) {
+    debugger
     let star = ''
     switch (rating) {
       case 1:
-        star = '../assets/yelp/yelp_stars/regular/regular_1.png'
+        star = './src/assets/yelp/yelp_stars/regular/regular_1.png'
         break
       case 1.5:
-        star = '../assets/yelp/yelp_stars/regular/regular_1_half.png'
+        star = './src/assets/yelp/yelp_stars/regular/regular_1_half.png'
         break
       case 2:
-        star = '../assets/yelp/yelp_stars/regular/regular_2.png'
+        star = './src/assets/yelp/yelp_stars/regular/regular_2.png'
         break
       case 2.5:
-        star = '../assets/yelp/yelp_stars/regular/regular_2_half.png'
+        star = './src/assets/yelp/yelp_stars/regular/regular_2_half.png'
         break
       case 3:
-        star = '../assets/yelp/yelp_stars/regular/regular_3.png'
+        star = './src/assets/yelp/yelp_stars/regular/regular_3.png'
         break
       case 3.5:
-        star = '../assets/yelp/yelp_stars/regular/regular_3_half.png'
+        star = './src/assets/yelp/yelp_stars/regular/regular_3_half.png'
         break
       case 4:
-        star = '../assets/yelp/yelp_stars/regular/regular_4.png'
+        star = './src/assets/yelp/yelp_stars/regular/regular_4.png'
         break
       case 4.5:
-        star = '../assets/yelp/yelp_stars/regular/regular_4_half.png'
+        star = './src/assets/yelp/yelp_stars/regular/regular_4_half.png'
         break
       case 5:
-        star = '../assets/yelp/yelp_stars/regular/regular_5.png'
+        star = './src/assets/yelp/yelp_stars/regular/regular_5.png'
         break
       default:
-        star = '../assets/yelp/yelp_stars/regular/regular_0.png'
+        star = './src/assets/yelp/yelp_stars/regular/regular_0.png'
     }
     return star
   }
